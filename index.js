0c87a32fc850f6a92a6b6647dd77fa12328a46b8
ashikmia0099 <ashikmia5686@gmail.com>
2025-01-05 23:14:10 +0600

    all setup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 // middleware set
 
-app.use(cors());
+app.use(cors({origin:['http://localhost:5173','https://learn-langauge.web.app']}));
 app.use(express.json());
 
 
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const LangaugesCollection = client.db('Learn_Langauge').collection('Langauges');
     const selectedTutorCollection = client.db('Learn_Langauge').collection('selected_Tutor');
@@ -164,7 +164,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
